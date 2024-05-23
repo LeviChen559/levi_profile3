c1a5738d9c1c2e65ad639429e44541662fb49e13
levi <levi@studypug.com>
2024-05-23 00:01:55 -0700

    update nav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import Nav from "@/components/nav";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between"> 
-    <Nav/>
+    <Nav type="header"/>
       <HeroImage/>
     </main>
   );
